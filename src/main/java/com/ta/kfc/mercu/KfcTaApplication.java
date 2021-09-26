a243b07f48cda5286a03650f08975eece6182635
yakub <yakub.andi@ovo.id>
2021-09-26 08:21:17 +0700

    initial commit

---
+++ src/main/java/com/ta/kfc/mercu/KfcTaApplication.java
@@ -0,0 +1,13 @@
+package com.ta.kfc.mercu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfcTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfcTaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ta/kfc/mercu/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ta.kfc.mercu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+//  @GetMapping("/index")
+//  public String showIndex(Model model) {
+//    return "index";
+//  }
+}
