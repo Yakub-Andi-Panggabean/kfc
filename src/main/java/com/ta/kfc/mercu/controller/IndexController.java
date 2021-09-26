05cc038146b93ecf17a43df97f16c7b841ed8de1
yakub.andi <yakub.jobs@gmail.com>
2021-09-26 08:31:16 +0700

    init

--- src/main/java/com/ta/kfc/mercu/controller/IndexController.java
+++ src/main/java/com/ta/kfc/mercu/controller/IndexController.java
@@ -6,8 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-//  @GetMapping("/index")
-//  public String showIndex(Model model) {
-//    return "index";
-//  }
+
 }
