8948d1f30a278c76c169ffde8471f91fad019933
Yakub A Panggabean <yakub.andi@ovo.id>
2021-12-04 00:41:04 +0700

    push latest changes

--- src/main/java/com/ta/kfc/mercu/interfaces/web/asset/AssetController.java
+++ src/main/java/com/ta/kfc/mercu/interfaces/web/asset/AssetController.java
@@ -1,10 +1,16 @@
 package com.ta.kfc.mercu.interfaces.web.asset;
 
+import com.ta.kfc.mercu.context.FastContext;
+import com.ta.kfc.mercu.infrastructure.db.orm.model.actor.UserDetail;
+import com.ta.kfc.mercu.infrastructure.db.orm.model.transaction.RequestOrderStatus;
 import com.ta.kfc.mercu.service.security.AuthorizationService;
+import com.ta.kfc.mercu.service.transaction.RequestOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class AssetController {
@@ -15,16 +21,28 @@ public class AssetController {
 
 
     private AuthorizationService authorizationService;
+    private RequestOrderService requestOrderService;
+    private FastContext context;
 
     @Autowired
-    public AssetController(AuthorizationService authorizationService) {
+    public AssetController(AuthorizationService authorizationService,
+                           RequestOrderService requestOrderService,
+                           FastContext context) {
         this.authorizationService = authorizationService;
+        this.requestOrderService = requestOrderService;
+        this.context = context;
     }
 
     @GetMapping({ASSET_VERIFICATION_PATH})
     public String getAssetVerificationPage(Model model) {
 
+        UserDetail userDetail = context.getUser().get().getUserDetail();
+
         model.addAttribute("template", "asset_verification");
+        model.addAttribute("orders", requestOrderService.findRequestOrderPerUser(userDetail)
+                .stream()
+                .filter(ro -> ro.getStatus() == RequestOrderStatus.SEND)
+                .collect(Collectors.toList()));
 
         return "index";
     }
