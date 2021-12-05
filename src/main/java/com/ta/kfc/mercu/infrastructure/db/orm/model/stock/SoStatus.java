c4d910b8b4a59d6f8e180be225b73ebef94050b0
Yakub A Panggabean <yakub.andi@ovo.id>
2021-12-05 22:43:42 +0700

    update so

---
+++ src/main/java/com/ta/kfc/mercu/infrastructure/db/orm/model/stock/SoStatus.java
@@ -0,0 +1,5 @@
+package com.ta.kfc.mercu.infrastructure.db.orm.model.stock;
+
+public enum SoStatus {
+    OK, NOT_OK
+}
--- src/main/java/com/ta/kfc/mercu/infrastructure/db/orm/model/stock/StockOpnameAsset.java
+++ src/main/java/com/ta/kfc/mercu/infrastructure/db/orm/model/stock/StockOpnameAsset.java
@@ -17,6 +17,40 @@ public class StockOpnameAsset {
     @OneToOne
     @JoinColumn(name = "asset_id")
     private Asset asset;
-    private AssetStatus assetStatus;
+
+    @Enumerated(EnumType.STRING)
+    private SoStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public StockOpnameDetail getStockOpnameDetail() {
+        return stockOpnameDetail;
+    }
+
+    public void setStockOpnameDetail(StockOpnameDetail stockOpnameDetail) {
+        this.stockOpnameDetail = stockOpnameDetail;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    public SoStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SoStatus status) {
+        this.status = status;
+    }
 }
 
