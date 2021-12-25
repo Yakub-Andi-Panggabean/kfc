0a84048f0803ef0415b8a183671b1267d7c0cec5
Yakub A Panggabean <yakub.andi@ovo.id>
2021-12-25 13:05:13 +0700

    finishing product report

--- src/main/java/com/ta/kfc/mercu/interfaces/web/report/ReportController.java
+++ src/main/java/com/ta/kfc/mercu/interfaces/web/report/ReportController.java
@@ -11,8 +11,10 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.ta.kfc.mercu.context.FastContext;
 import com.ta.kfc.mercu.dto.report.ProductParameter;
 import com.ta.kfc.mercu.dto.report.UnitParameter;
+import com.ta.kfc.mercu.infrastructure.db.orm.model.actor.UserDetail;
 import com.ta.kfc.mercu.infrastructure.db.orm.model.asset.Asset;
 import com.ta.kfc.mercu.infrastructure.db.orm.model.asset.ItemReceipt;
+import com.ta.kfc.mercu.infrastructure.db.orm.model.auth.User;
 import com.ta.kfc.mercu.infrastructure.db.orm.model.master.Product;
 import com.ta.kfc.mercu.infrastructure.db.orm.model.master.Unit;
 import com.ta.kfc.mercu.infrastructure.db.orm.model.transaction.RequestOrder;
@@ -20,6 +22,7 @@ import com.ta.kfc.mercu.interfaces.web.item.ItemModule;
 import com.ta.kfc.mercu.interfaces.web.order.OrderModule;
 import com.ta.kfc.mercu.service.asset.AssetService;
 import com.ta.kfc.mercu.service.master.MasterService;
+import com.ta.kfc.mercu.service.security.AuthenticationService;
 import com.ta.kfc.mercu.service.transaction.RequestOrderService;
 import com.ta.kfc.mercu.shared.CollectionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +56,7 @@ public class ReportController {
     private ServletContext servletContext;
     private CollectionHelper collectionHelper;
     private SpringTemplateEngine templateEngine;
+    private AuthenticationService authenticationService;
     private MasterService masterService;
     private FastContext fastContext;
 
@@ -63,10 +67,12 @@ public class ReportController {
                             FastContext fastContext,
                             AssetService assetService,
                             MasterService masterService,
+                            AuthenticationService authenticationService,
                             SpringTemplateEngine templateEngine,
                             CollectionHelper collectionHelper) {
 
         this.requestOrderService = requestOrderService;
+        this.authenticationService = authenticationService;
         this.servletContext = servletContext;
         this.collectionHelper = collectionHelper;
         this.templateEngine = templateEngine;
@@ -377,5 +383,198 @@ public class ReportController {
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(out.toByteArray());
     }
+
+
+    @GetMapping(REPORT_PATH + "/user/list")
+    public ResponseEntity<?> getAssetListPerproductPdfPage() throws UnsupportedEncodingException, DocumentException {
+
+        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        List<User> users = authenticationService.findAll();
+
+        Font font1 = new Font(Font.FontFamily.HELVETICA, 80, Font.BOLD
+                | Font.UNDERLINE);
+
+        Paragraph paragraph1 = new Paragraph();
+        paragraph1.add("User Report");
+        paragraph1.setAlignment(Element.ALIGN_CENTER);
+        paragraph1.setFont(font1);
+
+
+        PdfPTable table = new PdfPTable(6);
+
+        Font headFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.WHITE);
+
+        PdfPCell hcell;
+        hcell = new PdfPCell(new Phrase("Employee-id", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Firstname", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Lastname", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Department", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Unit", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Role", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        Font rowFont = FontFactory.getFont(FontFactory.HELVETICA, 10, BaseColor.BLACK);
+
+
+        for (User u : users) {
+            PdfPCell cell;
+
+            cell = new PdfPCell(new Phrase(u.getUserDetail().getCode(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+
+            cell = new PdfPCell(new Phrase(u.getUserDetail().getFirstName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+
+            cell = new PdfPCell(new Phrase(u.getUserDetail().getLastName()));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(u.getUserDetail().getDepartment().getName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(u.getUserDetail().getUnit().getUnitName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(u.getRoles().toString(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+        }
+
+        com.itextpdf.text.pdf.PdfWriter.getInstance(document, out);
+        document.open();
+        document.add(paragraph1);
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        document.add(table);
+        document.close();
+
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(out.toByteArray());
+    }
+
+
+    @GetMapping(REPORT_PATH + "/product/list")
+    public ResponseEntity<?> getProductListPdfPage() throws DocumentException {
+
+        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        List<Product> products = masterService.getAllProducts();
+
+        Font font1 = new Font(Font.FontFamily.HELVETICA, 80, Font.BOLD
+                | Font.UNDERLINE);
+
+        Paragraph paragraph1 = new Paragraph();
+        paragraph1.add("Product Report");
+        paragraph1.setAlignment(Element.ALIGN_CENTER);
+        paragraph1.setFont(font1);
+
+
+        PdfPTable table = new PdfPTable(4);
+
+        Font headFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.WHITE);
+
+        PdfPCell hcell;
+        hcell = new PdfPCell(new Phrase("Name", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Brand", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Model", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Type", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hcell.setBackgroundColor(BaseColor.RED);
+        table.addCell(hcell);
+
+        Font rowFont = FontFactory.getFont(FontFactory.HELVETICA, 10, BaseColor.BLACK);
+
+
+        for (Product u : products) {
+            PdfPCell cell;
+
+            cell = new PdfPCell(new Phrase(u.getName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+
+            cell = new PdfPCell(new Phrase(u.getBrand().getName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+
+            cell = new PdfPCell(new Phrase(u.getModel().getName(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(u.getProductType().toString(), rowFont));
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+        }
+
+        com.itextpdf.text.pdf.PdfWriter.getInstance(document, out);
+        document.open();
+        document.add(paragraph1);
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        document.add(table);
+        document.close();
+
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(out.toByteArray());
+    }
+
 
 }
